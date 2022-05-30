3a8583e9145ea51a4d50bd8fa7f4af35ba945883
João Gabriel <j.gabriel.21f@gmail.com>
2022-05-30 20:41:08 -0300

    criando tabela de codigos e servicos

---
+++ database/migrations/2022_05_08_215216_create_codigos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codigos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo');
+            $table->string('descricao');
+            $table->string('troca_obrigatoria');
+            $table->integer('value_ups_mono');
+            $table->integer('value_ups_tri');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codigos');
+    }
+};
---
+++ database/migrations/2022_05_30_203351_create_servicos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uc');
+            $table->date('data_execucao');
+            $table->integer('codigo_id')->unsigned();
+            $table->foreign('codigo_id')->references('id')->on('codigos');
+            $table->integer('responsavel_id')->unsigned();
+            $table->foreign('responsavel_id')->references('id')->on('users');
+            $table->string('status');
+            $table->string('data_baixa');
+            $table->integer('lancado_por_id')->unsigned();
+            $table->foreign('lancado_por_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
